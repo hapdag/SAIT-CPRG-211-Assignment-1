248c88428d5327eef020eb153b65499d856151e2
Si He <SiYuan.He@edu.sait.ca>
2024-06-25 13:38:41 -0600

    Added constructor and properties for refriderators

--- Assignment 1/Refriderator.cs
+++ Assignment 1/Refriderator.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace Assignment_1
 {
-    internal class Refriderator
+    internal class Refriderator : Appliance
     {
+        private int _height;
+        private int _width;
+        private int _numOfDoors;
+        public int Height { get { return _height; } set { _height = value; } }
+        public int Width { get { return _width; }set { _width = value; } }
+        public int NumOfDoors { get { return _numOfDoors; } set { _numOfDoors = value; } }
+        public Refriderator(long itemNumber, string brand, int quantity, double wattage, string color, double price, int height, int width, int numOfDoors) : base(itemNumber, brand, quantity, wattage, color, price)
+        {
+            _height = height;
+            _width = width;
+            _numOfDoors = numOfDoors;
+        }
+
+        public override void checkout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string formatForFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool isAvailable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string toString()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
