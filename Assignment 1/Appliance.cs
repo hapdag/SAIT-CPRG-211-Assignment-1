a3e123cb2d0991dc76f4c5b157a93c86cbb5b7b7
Si He <SiYuan.He@edu.sait.ca>
2024-06-25 13:01:06 -0600

    Added .gitignore, fleshed out abstract class.

--- Assignment 1/Appliance.cs
+++ Assignment 1/Appliance.cs
@@ -15,5 +15,18 @@ namespace Assignment_1
         private string _color;
         private double _price;
 
+        public long ItemNumber { get { return _itemNumber; } set { _itemNumber = value; } }
+        public string Brand { get { return _brand; } set { _brand = value; } }
+        public int Quantity { get { return _quantity; } set { _quantity = value; } }
+        public double Wattage { get { return _wattage; } set { _wattage = value; } }
+        public string Color { get { return _color; } set { _color = value; } }
+        public double Price { get { return _price; } set { _price = value; } }
+
+        public abstract bool isAvailable();
+        public abstract void checkout();
+        public abstract string formatForFile();
+        public abstract string toString();
+
+
     }
 }
