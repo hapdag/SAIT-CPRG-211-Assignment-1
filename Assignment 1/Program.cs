c89bddbfbbc10ccd4ca59c9c4e138e8d44ad759d
Si He <SiYuan.He@edu.sait.ca>
2024-06-25 14:03:47 -0600

    Added switch case for iterating through text list

--- Assignment 1/Program.cs
+++ Assignment 1/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,10 +13,35 @@ namespace Assignment_1
         static void Main(string[] args)
         {
             String[] fileData = Resources.appliances.Split('\n');
+            List<Appliance> appList = new List<Appliance>();
             foreach (String s in fileData) 
             {
-                String[] applianceData = s.Split(';');
-                Console.WriteLine(applianceData[0]); 
+                if (String.IsNullOrEmpty(s))
+                {
+                    continue;
+                }
+                else
+                {
+                    String[] applianceData = s.Split(';');
+                    switch (applianceData[0][0])
+                    {
+                        case '1':
+                            Console.WriteLine("fridge");
+                            break;
+                        case '2':
+                            Console.WriteLine("Vacuum");
+                            break;
+                        case '3':
+                            Console.WriteLine("Microwave");
+                            break;
+                        case '4':
+                            Console.WriteLine("Dishwasher");
+                            break;
+                        case '5':
+                            Console.WriteLine("Dishwasher");
+                            break;
+                    }
+                }
             }
         }
     }
