d56afa27047e58437f6230fa3d14da04522a5759
Si He <SiYuan.He@edu.sait.ca>
2024-06-25 12:46:19 -0600

    Inital commit with project files for assignment 1

---
+++ Assignment 1/Appliance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    internal abstract class Appliance
+    {
+        private long _itemNumber;
+        private string _brand;
+        private int _quantity;
+        private double _wattage;
+        private string _color;
+        private double _price;
+
+    }
+}
---
+++ Assignment 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using Assignment_1.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] fileData = Resources.appliances.Split('\n');
+            foreach (String s in fileData) 
+            {
+                String[] applianceData = s.Split(';');
+                Console.WriteLine(applianceData[0]); 
+            }
+        }
+    }
+}
